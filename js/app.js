230d13fe642875ea314b6f1fa7bd66d423425935
liamswork <137915577+liamswork@users.noreply.github.com>
2023-08-04 11:16:45 +0100

    -Side Nav Handling
    - toggle button for navbar.
    - Icon replaced, redundant button removed.
    -Transition effect fixed, was being overwritten by js and not working
    -Various debugging but cannot get functionality correct, will continue

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 const $sideNav = $(".sidenav");
+let isOpen = false;
 
-function openNav(){
-    $sideNav.css("transition", "ease 1s");
-    $sideNav.css("width", "100vw");
-    $sideNav.show();
-    $sideNav.css("z-index", "1000");
-}
-
-function closeNav(){
-    $sideNav.css("width", "0");
-    $sideNav.hide();
+function handleNav(){
+    if(isOpen === true){
+        $sideNav.css("transition", "0.5 ease");
+        $sideNav.toggleClass('navbar-expanded');
+        isOpen = false;
+        console.log("close nav");
+    }else if (isOpen === false){
+        $sideNav.css("transition", "0.5 ease");
+        $sideNav.toggleClass('navbar-expanded');
+        $sideNav.css("z-index", "1000");
+        isOpen = true;
+        console.log("open nav");
+    }
 }
