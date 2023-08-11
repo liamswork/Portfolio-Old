75f9c65f3c9a150c78a9a315d1a7543b3fdb72f7
liamswork <137915577+liamswork@users.noreply.github.com>
2023-08-11 20:06:20 +0100

    Feedback fixes/Owl carousel
    -Spacing on footer fixed.
    -Spacing between article title and content fixed
    -Newsletter button hover fixed.
    -Implemented carousel on sliding clients, description boxes still showing as intended.

---
+++ pages/Projects/NetmattersReflection/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-partners').owlCarousel({
+        margin:10,
+        loop:true,
+        autoWidth:true,
+        items: 9,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    })
+    $('.sliding-clients').owlCarousel({
+        margin:10,
+        loop:true,
+        autoWidth:true,
+        items: 9,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    })
+});
