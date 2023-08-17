4e426283c7a07dd3719b040bd13916d19009d8c8
liamswork <137915577+liamswork@users.noreply.github.com>
2023-08-17 22:39:26 +0100

    Sidenav Changes/Cookies
    -Added cookies preferences popup with local storage to remember whether buttons been clicked
    -Fixes to sidenav, changed sliding functionality, various fixes.

--- pages/Projects/NetmattersReflection/js/sidenav.js
+++ pages/Projects/NetmattersReflection/js/sidenav.js
@@ -1,33 +1,34 @@
-
-  var hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", function() {
-    console.log(event);
-    hamburger.classList.toggle("is-active");
-  });
-
 const $sideNav = $(".sidebar");
 const $mainWrapper = $(".main-wrapper")
 const $overlay = $(".overlay")
-
 let isOpen = false;
+let previousHamburger = null;
 
-function handleNav(){
-    let distanceDown = (window.scrollY)*-1;
-    if(isOpen === true){
+function handleNav(hamburger){
+    if(isOpen){
+        //Apply style classes which move the page and close the navbar.
         $sideNav.toggleClass('navbar-expanded');
         $mainWrapper.toggleClass('navbar-expanded');
         $overlay.toggleClass('shown');
         document.body.classList.toggle('navbar-expanded');
+
+        //Remove is-active from hamburger clicked to open the menu.
+        previousHamburger.classList.remove('is-active');
+
+        //Cleanup and debug can go here.
         isOpen = false;
-        console.log("close nav");
-    }
-    else if (isOpen === false)
-    {
+    }else{
+        //Apply style classes which move the page and open the navbar.
         document.body.classList.toggle('navbar-expanded');
         $sideNav.toggleClass('navbar-expanded');
         $mainWrapper.toggleClass('navbar-expanded');
         $overlay.toggleClass('shown');
+
+        //Add is-active to hamburger that was clicked to start this function.
+        hamburger.classList.add('is-active');
+
+        //Cleanup and debug
         isOpen = true;
-        console.log("open nav");
     }
+    previousHamburger = hamburger;
 }
